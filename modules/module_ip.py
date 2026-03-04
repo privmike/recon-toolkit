168b74587ea96b2ec00bc86d62698fb8a560ed76
Mike <131933991+privmike@users.noreply.github.com>
2026-03-04 18:18:29 +0700

    modul ip blm selsasi

---
+++ modules/module_ip.py
@@ -0,0 +1,35 @@
+import requests
+
+from utils.logger import log
+
+class IPModule:
+    def __init__(self, domain,config):
+        self.domain = domain
+        self.config = config
+        self.mode = self.config['settings'].get('execution_mode','default')
+        self.timeout = 5
+    def run(self):
+        log.info(f"Running IP dengan mode {self.mode} untuk {self.domain}")
+
+        method = [
+            ("IP-API", self.method_ip_api),
+            ("freeipapi", self.method_freeipapi)
+        ]
+
+    def method_ip_api(self):
+        try:
+            url = f"http://ip-api.com/json/{self.domain}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,query"
+            response = requests.get(url, timeout=self.timeout)
+            if response.status_code ==200:
+                data = response.json()
+                if data.get("status") == "success":
+                    return data
+            return None
+        except Exception as e :
+            log.debug(f"error di modul ip , metode ip-api: {str(e)}")
+            return None
+
+    def method_freeipapi(self):
+        try:
+
+            url = f"https://free.freeipapi.com/api/json/{self.domain}"
