6f89c2503fb86409b787acd397c3a1b3296206a2
Mike <131933991+privmike@users.noreply.github.com>
2026-05-20 14:38:01 +0700

    increase xnldorker timeout

--- modules/module_dorking.py
+++ modules/module_dorking.py
@@ -59,7 +59,7 @@ class DorkingModule:
             tmpFile = f"tmp_xnldorker_{category}.txt"
             cmd = ["xnldorker" , "-i",queryString, "-es","google,yandex","-o",tmpFile ,"-ow"]
             try:
-                process = subprocess.run(cmd, capture_output=True, text=True, timeout=600)
+                process = subprocess.run(cmd, capture_output=True, text=True, timeout=6000)
                 if process.returncode != 0:
                     log.error(f"Error running xnldorker: {process.stderr}")
                     return { "error":f"Error running xnldorker: {process.stderr}"}
